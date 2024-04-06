006be7516caedee4a65b8a1cac477402f30d39c4
George Andrinopoulos <geoandri@gmail.com>
2024-04-06 11:45:18 +0300

    First Commit

---
+++ src/main/java/org/geoandri/alexmod/SuperSword.java
@@ -0,0 +1,11 @@
+package org.geoandri.alexmod;
+
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
+
+public class SuperSword extends SwordItem {
+    public SuperSword(Tier p_43269_, int p_43270_, float p_43271_, Properties p_43272_) {
+        super(Tiers.NETHERITE, 1000, 0, new Properties());
+    }
+}
